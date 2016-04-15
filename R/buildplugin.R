2090ac04134c25c20cc8070a005a1121c18a3166
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-04-15 14:11:08 -0700

    initial commit of buildPlugin

---
+++ R/buildplugin.R
@@ -0,0 +1,32 @@
+isOlder2 <- function(target, ...){
+  #if (!file.exists(target)){ return(TRUE) }
+  any(file.mtime(target) < file.mtime(...))
+}
+
+#' Build Plugin
+#' 
+#' 
+#' @export
+#' @param pluginDir directory containing the plugin
+buildPlugin <- function(pluginDir = "."){
+  yxmc <- list.files(file.path(pluginDir, "Supporting_Macros"), pattern = ".yxmc$", full = T)
+  pluginName = tools::file_path_sans_ext(basename(yxmc))
+  guiFile <- file.path(pluginDir, sprintf("%sGui.html", pluginName))
+  configFile <- file.path(pluginDir, sprintf("%sConfig.xml", pluginName))
+  rFile <- list.files(file.path(pluginDir, "Supporting_Macros"), pattern = ".R$", full = T)
+  updated <- FALSE
+  if (isOlder2(guiFile, 'Gui/layout.html', 'Gui/overrides.yaml', yxmc)){
+    updated <- TRUE
+    message("Updating Gui.html and Config.xml")
+    createPluginFromMacro(pluginDir)
+  }
+  if (isOlder2(yxmc, rFile)){
+    updated <- TRUE
+    insertRcode(yxmc, rFile)
+  }
+  if (updated == FALSE){
+    message("Nothing to update...")
+  } else {
+    updateHtmlPlugin(pluginDir)
+  }
+}
