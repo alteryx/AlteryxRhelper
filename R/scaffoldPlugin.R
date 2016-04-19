1fdd16a325f9ee811089ed419fce2e50e5f95810
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-04-19 09:51:50 -0700

    add scaffold for app.min.js

--- R/scaffoldPlugin.R
+++ R/scaffoldPlugin.R
@@ -9,5 +9,12 @@ scaffoldPlugin <- function(pluginName){
   }
   cwd = getwd(); on.exit(setwd(cwd)); setwd(pluginName)
   scaffoldWorkflow(pluginName, outDir = 'Supporting_Macros', edit = F)
+  if (!file.exists('app.min.js')){
+    app = system.file('templates', 'app.min.js', package = 'AlteryxRhelper')
+    file.copy(app, ".")
+  }
+  if (!file.exists('app.css')){
+    file.create('app.css')
+  }
   createPluginFromMacro(".")
 }
