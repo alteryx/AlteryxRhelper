d291a243b7ab46484929deb21517165cba8d496a
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-04-20 16:09:04 -0700

    add generateConfigurationTable function.
    also fix a minor bug in buildPlugin.

--- R/buildplugin.R
+++ R/buildplugin.R
@@ -16,11 +16,14 @@ buildPlugin <- function(pluginDir = "."){
   rFile <- list.files(file.path(pluginDir, "Supporting_Macros"), pattern = ".R$", full.names = T)
   updated <- FALSE
   if (dir.exists('Gui')){
-    if (isOlder2(guiFile, 'Gui/layout.html', 'Gui/overrides.yaml', yxmc)){
-      updated <- TRUE
-      message("Updating Gui.html and Config.xml")
-      createPluginFromMacro(pluginDir)
-    }
+    to_update <- isOlder2(guiFile, 'Gui/layout.html', 'Gui/overrides.yaml', yxmc)
+  } else {
+    to_update <-  isOlder2(guiFile, yxmc)
+  }
+  if (to_update){
+    updated <- TRUE
+    message("Updating Gui.html and Config.xml")
+    createPluginFromMacro(pluginDir)
   }
   if (isOlder2(yxmc, rFile)){
     updated <- TRUE
@@ -31,4 +34,25 @@ buildPlugin <- function(pluginDir = "."){
   } else {
     updateHtmlPlugin(pluginDir)
   }
+}
+
+#' Generate table of configuration items
+#' 
+#' 
+#' @export
+#' @param yxmcFile path to macro
+generateConfigurationTable <- function(yxmcFile){
+  d <- yxmc2yaml(yxmcFile)
+  d2 <- plyr::ldply(names(d), function(x){
+    d_ = d[[x]]
+    foo = function(x){x[is.null(x)] <- ""; x}
+    c(
+      dataName = x, 
+      label = ifelse(is.null(d_$label), "", d_$label),
+      default = ifelse(is.null(d_$default), "", d_$default),
+      values = ifelse(is.null(d_$values), "", 
+        jsonlite::toJSON(names(d_$values), auto_unbox = TRUE)
+      )
+    )
+  })
 }
