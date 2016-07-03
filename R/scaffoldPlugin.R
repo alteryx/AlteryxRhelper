e1360f217cf6a6eceb8ee98ab126598c5d128549
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-07-03 10:05:38 -0700

    update scaffoldPlugin to support new structure.

--- R/scaffoldPlugin.R
+++ R/scaffoldPlugin.R
@@ -5,10 +5,29 @@
 #' @param pluginName name of the plugin
 scaffoldPlugin <- function(pluginName){
   if (!dir.exists(pluginName)){
-    dir.create(pluginName)
+    #dir.create(pluginName)
+    from <- system.file('pluginTemplate', package = 'AlteryxRhelper')
+    copy_dir(from, pluginName)
+    file.rename(
+      file.path(pluginName, 'template.txt'), 
+      file.path(pluginName, paste0(pluginName, '.Rproj'))
+    )
+    file.rename(
+      file.path(pluginName, 'gitignore'), 
+      file.path(pluginName, '.gitignore')
+    )
+    readmes <- list.files(
+      pluginName, pattern = 'README', recursive = TRUE, full = TRUE
+    )
+    for (readme in readmes){
+      updateReadme(readme, pluginName)
+    }
   }
   cwd = getwd(); on.exit(setwd(cwd)); setwd(pluginName)
   scaffoldWorkflow(pluginName, outDir = 'Supporting_Macros', edit = F)
+  if (!dir.exists('Supporting_Files')){
+    dir.create('Supporting_Files')
+  }
   if (!file.exists('app.min.js')){
     app = system.file('templates', 'app.min.js', package = 'AlteryxRhelper')
     file.copy(app, ".")
@@ -17,4 +36,16 @@ scaffoldPlugin <- function(pluginName){
     file.create('app.css')
   }
   createPluginFromMacro(".")
+  message("Initializing a git repository...")
+  r <- git2r::init(".")
+  # paths <- unlist(git2r::status(r))
+  # git2r::add(r, paths)
+  # git2r::commit(r, 'Initial Commit')
+}
+
+
+updateReadme <- function(readme, pluginName){
+  input <- paste(readLines(readme, warn = F), collapse = '\n')
+  output <- whisker::whisker.render(input, list(pluginName = pluginName))
+  cat(output, file = readme)
 }
