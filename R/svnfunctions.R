38d8cacb3b089259d7dae83d8d6b2d7136524462
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-10-10 16:40:52 -0700

    update SVN functions.

--- R/svnfunctions.R
+++ R/svnfunctions.R
@@ -4,17 +4,19 @@
 #' @param srcPkg path to source package
 #' @param install whether or not to actually install dependencies
 #' @export
-installToSvn <- function(srcPkg, install = TRUE){
+installToSvn <- function(srcPkg, to_install = TRUE){
   srcPkg <- normalizePath(srcPkg)
   rdirs <- getAyxSvnRDirs()
   svnLibDir <- rdirs$lib
   d <- read.dcf(file.path(srcPkg, "DESCRIPTION"))
   imports <- gsub("\\n", "", strsplit(d[,'Imports'], ",")[[1]])
+  ayxPackages <- c("AlteryxPrescriptive", "AlteryxRDataX", "AlteryxRviz")
+  imports <- setdiff(imports, ayxPackages)
   deps <- miniCRAN::pkgDep(imports, suggests = FALSE)
   installed <- rownames(installed.packages(svnLibDir))
   depsToInstall <- setdiff(deps, installed)
   install <- devtools::install
-  if (install){
+  if (to_install){
     withr::with_libpaths(svnLibDir, {
       if (length(depsToInstall) > 0){
         message(
@@ -78,11 +80,17 @@ saveReadme <- function(save = TRUE){
 #' @param svnDir svn directory
 #' @export
 getAyxSvnDirs <- function(svnDir = getOption("alteryx.svndir")){
-  svnDir <- 'C:/Users/ramnath/Desktop/Alteryx10.6_Predictive_Development'
+  svnDir <- 'C:\\Users\\ramnath\\Desktop\\SVN_Full_Repos\\Predictive_Development'
   list(
-    htmlplugin = file.path(svnDir, 'AlteryxRPlugin', 'HtmlPlugins'),
-    macro = file.path(svnDir, 'RuntimeData', 'Macros', 'Supporting_Macros')
+    htmlplugin = file.path(svnDir, "Alteryx", "Plugins", "AlteryxRPlugin", 
+                           "HtmlPlugins"),
+    macro = NULL
   )
+}
+
+#' @export
+copyHtmlPluginToSvn <- function(pluginDir = "."){
+  copyHtmlPlugin(ayxDir = getAyxSvnDirs())
 }
 
 #' Get Alteryx R SVN Directories
@@ -91,8 +99,10 @@ getAyxSvnDirs <- function(svnDir = getOption("alteryx.svndir")){
 #' @param svnDir svn directory
 #' @export
 getAyxSvnRDirs <- function(svnDir = getOption("alteryx.svndir")){
+  svnDir <- 'C:\\Users\\ramnath\\Desktop\\SVN_Full_Repos\\Predictive_Development'
+  rPath <- file.path(svnDir, "3rdParty", "R")
   list(
-    lib = file.path(svnDir, 'R', 'R_Installed_Files', 'R-3.2.3', 'library'),
-    installer = file.path(svnDir, 'R', 'Installer')
+    lib = file.path(rPath, 'R_Installed_Files', 'R-3.2.3', 'library'),
+    installer = file.path(rPath, 'Installer')
   )
 }
