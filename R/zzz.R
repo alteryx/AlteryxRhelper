359bf08e29539553a410a42eedd457c7a677a4cd
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-11-04 10:31:42 -0700

    minor updates to package startup.

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,17 @@
 .onLoad <- function(libname, pkgname){
   if (!('package:AlteryxRDataX' %in% search())){
-    #packageStartupMessage('Setting AlteryxFullUpdate to FALSE')
-    AlteryxFullUpdate <<- FALSE
+    # packageStartupMessage('Setting AlteryxFullUpdate to FALSE')
+    # AlteryxFullUpdate <<- FALSE
     if (.Platform$OS.type == "windows"){
       alteryx.path = "C:/Program Files/Alteryx"
+      dev.dir = "Z:/SNIPPETS"
       options('alteryx.svndir' = 'C:/Users/ramnath/Desktop/Alteryx10.6_Predictive_Development')
     } else {
       alteryx.path = "/Volumes/C/Program Files/Alteryx"
+      dev.dir = "~/Desktop/SNIPPETS"
     }
     options(alteryx.path = alteryx.path)
+    options(dev.dir = dev.dir)
   } else {
     # options(error = dump_and_quit)
   }
