1dcaaa5dcf70d8ea04d0a5661c3c9fdf226e042a
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-11-04 10:30:22 -0700

    comment out adding config.xml
    there are some issue currently. will add this code back once they are resolved.

--- R/main.R
+++ R/main.R
@@ -154,9 +154,9 @@ createYXI <- function(pluginDir = ".", toDir = "."){
   )
   yxi <- file.path(toDir, paste0(pluginName, '.yxi'))
   zip(yxi, filesToCopy, flags = "")
-  tf <- file.path(tempdir(), 'config.xml')
-  saveXML(makeConfigFile(file.path(pluginName, pluginFiles$macro)), file = tf)
-  zip(yxi, tf, flags = '-j')
+  #tf <- file.path(tempdir(), 'config.xml')
+  #saveXML(makeConfigFile(file.path(pluginName, pluginFiles$macro)), file = tf)
+  #zip(yxi, tf, flags = '-j')
 }
 
 #' @export
