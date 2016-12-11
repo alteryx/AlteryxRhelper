39ec4549da1dac45363ba8832bad6d9df7e58eb3
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-10 16:56:35 -0800

    dont export some utility functions.

--- R/utils.R
+++ R/utils.R
@@ -70,11 +70,7 @@ fieldSize = function(fieldType){
 #   }
 # }
 
-#' Template
-#' 
-#' 
-#' @export
-#' @param template template
+# template template
 aTemplate = function(template){
   system.file('templates', template, package = 'AlteryxRhelper')
 }
@@ -82,10 +78,7 @@ aTemplate = function(template){
 
 
 
-#' Dump and quit on error
-#' 
-#' 
-#' @export
+# Dump and quit on error
 dumpAndQuit <- function() {
   wd = getOption('alteryx.wd')
   # Save debugging info to file last.dump.rda
@@ -156,31 +149,6 @@ dirNames <- function(){
   )
 }
 
-#' Extract Icon
-#' 
-#' @param yxmc path to yxmc file
-#' @param out icon file to write out to
-extractIcon <- function(yxmc, out){
-  doc <- xmlInternalTreeParse(yxmc)
-  root <- xmlRoot(doc)
-  macroImg <- xmlValue(getNodeSet(root, '//MacroImage')[[1]])
-  x <- base64enc::base64decode(what = macroImg)
-  writeBin(x, out)
-}
-
-#' Stop
-#' 
-#' @param msg message
-#' @param ... extra arguments to pass on to stop.Alteryx
-#' @export
-stop.Alteryx <- function(msg, ...){
-  if (inAlteryx()){
-    AlteryxRDataX::stop.Alteryx(msg, ...)
-  } else {
-    stop(msg)
-  }
-}
-
 #' Generate table of configuration items
 #' 
 #' 
@@ -195,9 +163,10 @@ generateConfigurationTable <- function(yxmcFile){
     c(
       dataName = x, 
       label = ifelse(is.null(d_$label), "", d_$label),
-      default = ifelse(is.null(d_$default), 
-                       ifelse(is.null(d_$placeholder), "",  d_$placeholder),
-                       d_$default
+      default = ifelse(
+        is.null(d_$default), 
+        ifelse(is.null(d_$placeholder), "",  d_$placeholder),
+        d_$default
       ),
       values = ifelse(is.null(d_$values), "", 
         gsub(",", ", ", jsonlite::toJSON(names(d_$values), auto_unbox = TRUE))
@@ -213,32 +182,6 @@ copy_dir <- function (from, to) {
     file.copy(list.files(from, full.names = T), to, recursive = TRUE)
   }
 }
-
-#' Make a circular or square icon and save it to a png file.
-#'
-#' 
-#' @param iconPath path to save icon to
-#' @param shape shape of the icon (circle or rect)
-#' @param fill fill color
-#' @param label a label to use for the icon
-#' @import grid
-#' @export
-makeIcon <- function(iconPath, shape = 'circle', fill = sample(colors(), 1), 
-    label = NULL){
-  png(iconPath, width = 48, height = 48, units = 'px')
-  vp <- viewport(x=0.5,y=0.5,width=1, height=1)
-  pushViewport(vp)
-  if (shape == 'circle'){
-    grid.circle(x=0.5, y=0.5, r=0.45, gp = gpar(fill = fill))
-  } else {
-    grid.rect(x = 0.5, y = 0.5, width = 0.9, height = 0.9, gp = gpar(fill = fill))
-  }
-  if (!is.null(label)){
-    grid.text(label, gp = gpar(col = 'white', cex = 1.5))
-  }
-  dev.off()
-}
-
 
 updateReadme <- function(readme, pluginName){
   input <- paste(readLines(readme, warn = F), collapse = '\n')
@@ -257,3 +200,24 @@ renderReadmes <- function(pluginDir = ".", ...){
     }
   })
 }
+
+with_dir_ <- function (new, code) {
+  old <- setwd(dir = new)
+  on.exit(setwd(old))
+  force(code)
+}
+
+bool_map <- function(x){
+  c(True = TRUE, False = FALSE)[[x]]
+}
+
+toKeyValuePairs <- function(x){
+  y <- strsplit(x, "\n")[[1]]
+  if (grepl(":", y[1])){
+    y <- strsplit(y, ":\\s*")
+    as.list(setNames(sapply(y, '[[', 1), sapply(y, '[[', 2)))
+  } else {
+    as.list(setNames(y, y))
+  }
+}
+
