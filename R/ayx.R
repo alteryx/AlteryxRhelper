d469b67501135426a23e01253d7b3863b2b6e439
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-12-10 16:50:41 -0800

    add docs and reorganize order of functions.

--- R/ayx.R
+++ R/ayx.R
@@ -1,3 +1,101 @@
+#' Write Gui HTML for plugin
+#' 
+#'  @param pluginDir directory containing the plugin.
+#'  @param htmlFile html file to write the Gui to
+#'  @param overrides a list of overrides for widgets 
+#' @export
+writeGuiHtml <- function(pluginDir, htmlFile = NULL, overrides = NULL){
+  dirs <- dirNames()
+  pluginDir <- normalizePath(pluginDir)
+  pluginName <- basename(pluginDir)
+  if (is.null(htmlFile)){
+    htmlFile <- file.path(pluginDir, sprintf("%sGui.html", pluginName))
+  }
+  yxmcFile <- file.path(
+    pluginDir, dirs$macros, sprintf("%s.yxmc", pluginName)
+  )
+  layoutDir <- file.path(pluginDir, dirs$extras, 'Gui')
+  if (!dir.exists(layoutDir)){
+    dir.create(layoutDir)
+  }
+  layoutFile = file.path(layoutDir, 'layout.html.sample')
+  x1 <- yxmc2yaml(yxmcFile)
+  x1b <- paste(
+    c(
+      "<fieldset>", 
+      "<legend>Configuration</legend>",
+      paste0("{{ ", "`", names(x1), "`", " }}"), 
+      "</fieldset>\n"
+    ), 
+    collapse = '\n'
+  )
+  cat(x1b, file = layoutFile)
+  ov <- file.path(pluginDir, dirs$extras, 'Gui', 'overrides.yaml')
+  if (file.exists(ov)){
+    overrides <- yaml::yaml.load_file(ov)
+  }
+  if (!is.null(overrides)){
+    x1 <- modifyList(x1, overrides)
+  }
+  x2 <- makePage(x1)
+  x3 <- makeGuiHtml(x2, pluginName = pluginName)
+  cat(as.character(x3), file = htmlFile)
+}
+
+#' Write Gui HTML for plugin based on a layout
+#' 
+#' @inheritParams writeGuiHtml
+#' @export
+writeGuiHtmlFromLayout <- function(pluginDir, htmlFile = NULL, overrides = NULL){
+  dirs <- dirNames()
+  pluginDir <- normalizePath(pluginDir)
+  pluginName <- basename(pluginDir)
+  if (is.null(htmlFile)){
+    htmlFile <- file.path(pluginDir, sprintf("%sGui.html", pluginName))
+  }
+  mylayout <- paste(
+    readLines(file.path(pluginDir, dirs$extras, 'Gui', 'layout.html')), 
+    collapse = '\n'
+  )
+  yxmcFile <- file.path(
+    pluginDir, dirs$macros, sprintf("%s.yxmc", pluginName)
+  )
+  x1 <- yxmc2yaml(yxmcFile)
+  ov <- file.path(pluginDir, dirs$extras, "Gui", "overrides.yaml")
+  if (file.exists(ov)){
+    overrides <- yaml::yaml.load_file(ov)
+  }
+  if (!is.null(overrides)){
+    x1 <- modifyList(x1, overrides)
+  }
+  x1b <- lapply(seq_along(x1), function(i){
+    x1[[i]]$id = names(x1)[i]
+    x1[[i]]
+  })
+  names(x1b) <- names(x1)
+  w = renderAyxWidgets(x1b)
+  names(w) = names(x1b)
+  
+  htmlTextTemplate = function(...){
+    htmlTemplate(text_ = mylayout, ...)
+  }
+  x2 <- do.call(htmlTextTemplate, w)
+  x3 <- makeGuiHtml(x2, pluginName = pluginName)
+  cat(as.character(x3), file = htmlFile)
+}
+
+
+makeGuiHtml <- function(widgets, pluginName = "", template = NULL){
+  if (is.null(template)) {
+    template <- system.file(
+      'templates', 'GuiTemplate.html', package = 'AlteryxRhelper'
+    )
+  }
+  gui <- htmltools::htmlTemplate(template, widgets = widgets, title = pluginName)
+  return(gui)
+}
+
+
 ayxOption <- function(x){
   htmltools::tag('alteryx-option', x)
 }
@@ -5,6 +103,61 @@ ayxOption <- function(x){
 ayxWidgetWithProps <- function(x){
   d <- jsonlite::toJSON(Filter(Negate(is.null), x), auto_unbox = TRUE)
   htmltools::tag('alteryx-pluginwidget', list(`data-props` = d))
+}
+
+# yxmcFile <- system.file('templates', 'sample1.yxmc', package = 'AlteryxRhelper')
+# config <- yxmc2yaml(yxmcFile)
+# renderAyxWidgets(config)
+renderAyxWidgets <- function(config){
+  d2 <- lapply(seq_along(config), function(i){
+    config[[i]]$dataName = names(config)[i]; 
+    config[[i]]
+  })
+  do.call(tagList, lapply(d2, ayxPluginWidget))
+}
+
+makePage <- function(config, layout = NULL){
+  config <- lapply(seq_along(config), function(i){
+    config[[i]]$id = names(config)[i]
+    config[[i]]$dataName = names(config)[i]; 
+    config[[i]]
+  })
+  makeFieldSet <- function(x, id){
+    tags$fieldset(id = id,
+      tags$legend(id),
+      do.call(tagList, lapply(x, function(x){
+        d_ <- config[[x]]; d_$dataName = x;
+        ayxPluginWidget(d_)
+      }))             
+    )
+  }
+  if (!is.null(layout)){
+    layout <- yaml::yaml.load(layout)
+    do.call(tagList, lapply(names(layout), function(k){
+      makeFieldSet(layout[[k]], k)
+    }))
+  } else {
+    tags$fieldset(
+      tags$legend('Configuration'),
+      do.call(tagList, lapply(config, ayxPluginWidget))
+    ) 
+  }
+}
+
+renderToggleBar <- function(d){
+  div(class = 'clearfix togglebar', id = paste0('id-', d$group),
+    div(class = 'label', style='float:left;', d$label),
+    div(style = 'float:right;', div(class = 'toggletabs',
+      lapply(names(d$values), function(k){
+        cl = if (d$default == k) {
+          'toggletab is-tab-selected' 
+        } else {
+          'toggletab'
+        }
+        div(class = cl, `data-page` = k, id = paste0('id-', k), d$values[[k]])
+      })                                
+    ))
+  )
 }
 
 # Alteryx Plugin Widget
@@ -50,127 +203,4 @@ ayxPluginWidget = function(x){
     },
     tag('alteryx-pluginwidget', x)
   )
-}
-
-
-# yxmcFile <- system.file('templates', 'sample1.yxmc', package = 'AlteryxRhelper')
-# config <- yxmc2yaml(yxmcFile)
-# renderAyxWidgets(config)
-renderAyxWidgets <- function(config){
-  d2 <- lapply(seq_along(config), function(i){
-    config[[i]]$dataName = names(config)[i]; 
-    config[[i]]
-  })
-  do.call(tagList, lapply(d2, ayxPluginWidget))
-}
-
-makePage <- function(config, layout = NULL){
-  config <- lapply(seq_along(config), function(i){
-    config[[i]]$id = names(config)[i]
-    config[[i]]$dataName = names(config)[i]; 
-    config[[i]]
-  })
-  makeFieldSet <- function(x, id){
-    tags$fieldset(id = id,
-      tags$legend(id),
-      do.call(tagList, lapply(x, function(x){
-        d_ <- config[[x]]; d_$dataName = x;
-        ayxPluginWidget(d_)
-      }))             
-    )
-  }
-  if (!is.null(layout)){
-    layout <- yaml::yaml.load(layout)
-    do.call(tagList, lapply(names(layout), function(k){
-      makeFieldSet(layout[[k]], k)
-    }))
-  } else {
-    tags$fieldset(
-      tags$legend('Configuration'),
-      do.call(tagList, lapply(config, ayxPluginWidget))
-    ) 
-  }
-}
-
-makeGuiHtml <- function(widgets, pluginName = "", template = NULL){
-  if (is.null(template)) {
-    template <- system.file('templates', 'GuiTemplate.html', package = 'AlteryxRhelper')
-  }
-  gui <- htmltools::htmlTemplate(template, widgets = widgets, title = pluginName)
-  return(gui)
-}
-
-
-writeGuiHtml <- function(pluginDir, htmlFile = NULL, overrides = NULL){
-  dirs <- dirNames()
-  pluginDir <- normalizePath(pluginDir)
-  pluginName <- basename(pluginDir)
-  if (is.null(htmlFile)){
-    htmlFile <- file.path(pluginDir, sprintf("%sGui.html", pluginName))
-  }
-  yxmcFile <- file.path(
-    pluginDir, dirs$macros, sprintf("%s.yxmc", pluginName)
-  )
-  layoutDir <- file.path(pluginDir, dirs$extras, 'Gui')
-  if (!dir.exists(layoutDir)){
-    dir.create(layoutDir)
-  }
-  layoutFile = file.path(layoutDir, 'layout.html.sample')
-  x1 <- yxmc2yaml(yxmcFile)
-  x1b <- paste(
-    c(
-      "<fieldset>", 
-      "<legend>Configuration</legend>",
-      paste0("{{ ", "`", names(x1), "`", " }}"), 
-      "</fieldset>\n"
-    ), 
-    collapse = '\n'
-  )
-  cat(x1b, file = layoutFile)
-  if (file.exists(ov <- file.path(pluginDir, dirs$extras, 'Gui', 'overrides.yaml'))){
-    overrides <- yaml::yaml.load_file(ov)
-  }
-  if (!is.null(overrides)){
-    x1 <- modifyList(x1, overrides)
-  }
-  x2 <- makePage(x1)
-  x3 <- makeGuiHtml(x2, pluginName = pluginName)
-  cat(as.character(x3), file = htmlFile)
-}
-
-writeGuiHtmlFromLayout <- function(pluginDir, htmlFile = NULL, overrides = NULL){
-  dirs <- dirNames()
-  pluginDir <- normalizePath(pluginDir)
-  pluginName <- basename(pluginDir)
-  if (is.null(htmlFile)){
-    htmlFile <- file.path(pluginDir, sprintf("%sGui.html", pluginName))
-  }
-  mylayout <- paste(
-    readLines(file.path(pluginDir, dirs$extras, 'Gui', 'layout.html')), 
-    collapse = '\n'
-  )
-  yxmcFile <- file.path(
-    pluginDir, dirs$macros, sprintf("%s.yxmc", pluginName)
-  )
-  x1 <- yxmc2yaml(yxmcFile)
-  if (file.exists(ov <- file.path(pluginDir, dirs$extras, "Gui", "overrides.yaml"))){
-    overrides <- yaml::yaml.load_file(ov)
-  }
-  if (!is.null(overrides)){
-    x1 <- modifyList(x1, overrides)
-  }
-  x1b <- lapply(seq_along(x1), function(i){
-    x1[[i]]$id = names(x1)[i]
-    x1[[i]]
-  })
-  names(x1b) <- names(x1)
-  w = renderAyxWidgets(x1b)
-  names(w) = names(x1b)
-  
-  htmlTextTemplate = function(...){
-    htmlTemplate(text_ = mylayout, ...)
-  }
-  x2 <- do.call(htmlTextTemplate, w)
-  x3 <- makeGuiHtml(x2, pluginName = pluginName)
-  cat(as.character(x3), file = htmlFile)
 }
